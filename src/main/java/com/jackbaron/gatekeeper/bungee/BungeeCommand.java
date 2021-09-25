b63d2503276a2ac5983ea6f04c44f18f788c2727
Jack Baron <jackmbaron@gmail.com>
2021-09-25 18:33:56 +0100

    set slots

--- src/main/java/com/jackbaron/gatekeeper/bungee/BungeeCommand.java
+++ src/main/java/com/jackbaron/gatekeeper/bungee/BungeeCommand.java
@@ -5,8 +5,12 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Subcommand;
+import com.jackbaron.gatekeeper.common.Platform;
+import com.jackbaron.gatekeeper.common.SlotManager;
 import net.md_5.bungee.api.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import static com.jackbaron.gatekeeper.bungee.BungeePlugin.LOGGER;
 
 @CommandAlias("gatekeeperbungee")
 public final class BungeeCommand extends BaseCommand {
@@ -24,7 +28,15 @@ public final class BungeeCommand extends BaseCommand {
 
     @Subcommand("setslots")
     @CommandPermission("gatekeeper.setslots")
-    public void onSetSlots(final @NotNull CommandSender sender) {
+    public void onSetSlots(final @NotNull CommandSender sender, final int slots) {
+        assert LOGGER != null;
+
         // TODO
+        try {
+            SlotManager.SetSlots(Platform.BUNGEE, slots);
+        } catch (ReflectiveOperationException e) {
+            LOGGER.info("Failed to change slots!");
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/jackbaron/gatekeeper/paper/PaperCommand.java
+++ src/main/java/com/jackbaron/gatekeeper/paper/PaperCommand.java
@@ -5,8 +5,12 @@ import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Default;
 import co.aikar.commands.annotation.Subcommand;
+import com.jackbaron.gatekeeper.common.Platform;
+import com.jackbaron.gatekeeper.common.SlotManager;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import static com.jackbaron.gatekeeper.paper.PaperPlugin.LOGGER;
 
 @CommandAlias("gatekeeperbungee")
 public final class PaperCommand extends BaseCommand {
@@ -24,7 +28,15 @@ public final class PaperCommand extends BaseCommand {
 
     @Subcommand("setslots")
     @CommandPermission("gatekeeper.setslots")
-    public void onSetSlots(final @NotNull CommandSender sender) {
+    public void onSetSlots(final @NotNull CommandSender sender, final int slots) {
+        assert LOGGER != null;
+
         // TODO
+        try {
+            SlotManager.SetSlots(Platform.PAPER, slots);
+        } catch (ReflectiveOperationException e) {
+            LOGGER.info("Failed to change slots!");
+            e.printStackTrace();
+        }
     }
 }
